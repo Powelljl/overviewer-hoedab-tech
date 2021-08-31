c89850ce7f260b339aa2c00a15a09fbc0c2940d6
Josh Powell <josh@joshua.wales>
2021-08-31 02:19:28 +0100

    updated world 31/08/2021 02:19

--- README.md
+++
@@ -1,1 +0,0 @@
-overviewer-hoedab-tech
--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1630366587",
+        "cacheTag": "1630372548",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1630363318,
+            "last_rendertime": 1630372388,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
